96605e59fe491479b313d89e0bd683fe4904659f
fuzzyduck <174277889+fuzzyduck03a@users.noreply.github.com>
2024-08-23 22:43:15 -0600

    Add a page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'pageTitle',
+  description: 'test sandbox2'
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
