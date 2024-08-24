edad2dbe96873c14239bc3568543073208910fb0
fuzzyduck <174277889+fuzzyduck03a@users.noreply.github.com>
2024-08-23 23:13:58 -0600

    Add some material UI theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+
 export const metadata = {
   title: 'pageTitle',
   description: 'test sandbox2'
@@ -6,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
